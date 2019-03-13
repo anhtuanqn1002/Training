54b2719056d985a2c2d8bb7e13d343484719a228
Anh Tuan <anhtuanqn1002@gmail.com>
2019-03-13 22:47:16 +0700

    Init

---
+++ TrainingAll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainingAll
+//
+//  Created by Anh Tuan on 3/13/19.
+//  Copyright © 2019 tuan.nva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
