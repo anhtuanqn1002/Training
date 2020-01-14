528345a9fe7766f9dea9f9f08192d2a1bb2610fc
Tuan Nguyen <>
2020-01-14 16:29:57 +0700

    fastlane

---
+++ TrainingAll/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  TrainingAll
+//
+//  Created by Tuan Nguyen on 11/3/19.
+//  Copyright © 2019 tuan.nva. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- TrainingAll/ViewController.swift
+++ TrainingAll/ViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let datasource: [Int] = [Int](0...53)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return datasource.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.titleLabel.text = "\(datasource[indexPath.row])"
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {}
+
+class MyLayout: UICollectionViewLayout {
+    
+    var attributed: [IndexPath: UICollectionViewLayoutAttributes] = [:]
+    var contentSize: CGSize!
+    
+    override func prepare() {
+        super.prepare()
+        attributed.removeAll()
+        
+        
+        
+        let attribute = UICollectionViewLayoutAttributes(forCellWith: IndexPath(item: 0, section: 0))
+        attribute.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        attributed[IndexPath(row: 0, section: 0)] = attribute
+        self.contentSize = CGSize(width: 400, height: 400)
+    }
+    
+    override var collectionViewContentSize: CGSize {
+        return contentSize
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var layoutAttributes = [UICollectionViewLayoutAttributes]()
+        
+        for attributes in self.attributed {
+            if (attributes.value as! UICollectionViewLayoutAttributes).frame.intersects(rect ) {
+                layoutAttributes.append((attributes.value as! UICollectionViewLayoutAttributes))
+            }
+        }
+        return layoutAttributes
+    }
+}
