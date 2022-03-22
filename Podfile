c9dc536e6f792d956c633604a922800221ae2fb9
Tuan Nguyen <>
2022-03-22 14:51:46 +0700

    PHAsset: lấy thông tin image từ library. Trên iOS 14 thì dùng PHPickerViewController, còn thấp hơn thì UIPickerViewController. Lưu ý PHImageManager.default().requestImageData(for sẽ deprecated trên iOS 13. Dùng PHImageManager.default().requestImageDataAndOrientation(for thay thế trên iOS 14+

--- Podfile
+++ Podfile
@@ -5,11 +5,4 @@ target 'TrainingAll' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for TrainingAll
-  # add the Firebase pod for Google Analytics
-  pod 'Firebase/Analytics'
-  # add pods for any other desired Firebase products
-  # https://firebase.google.com/docs/ios/setup#available-pods
-  pod 'Firebase/RemoteConfig'
-
 end
--- TrainingAll/ViewController.swift
+++ TrainingAll/ViewController.swift
@@ -5,68 +5,55 @@
 //  Created by Anh Tuan on 3/13/19.
 //  Copyright © 2019 tuan.nva. All rights reserved.
 //
-
 import UIKit
-
-class ViewController: UIViewController {
-
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    let datasource: [Int] = [Int](0...53)
-    
+import Photos
+import PhotosUI
+class ViewController: UIViewController, UIImagePickerControllerDelegate, PHPickerViewControllerDelegate, UINavigationControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.dataSource = self
-        collectionView.delegate = self
         
-        RCValues.shared.fetchCloudValues()
-        print("==> \(RCValues.shared.getValue(.mainColor).stringValue)")
-    }
-}
-
-extension ViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return datasource.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
-        cell.titleLabel.text = "\(datasource[indexPath.row])"
-        return cell
-    }
-}
-
-extension ViewController: UICollectionViewDelegate {}
-
-class MyLayout: UICollectionViewLayout {
-    
-    var attributed: [IndexPath: UICollectionViewLayoutAttributes] = [:]
-    var contentSize: CGSize!
-    
-    override func prepare() {
-        super.prepare()
-        attributed.removeAll()
-        
-        
-        
-        let attribute = UICollectionViewLayoutAttributes(forCellWith: IndexPath(item: 0, section: 0))
-        attribute.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        attributed[IndexPath(row: 0, section: 0)] = attribute
-        self.contentSize = CGSize(width: 400, height: 400)
+    @IBAction func tapButon(_ sender: Any) {
+        if #available(iOS 14, *) {
+            let config = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+            let picker = PHPickerViewController(configuration: config)
+            picker.delegate = self
+            present(picker, animated: true, completion: nil)
+        } else {
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.mediaTypes = ["public.image", "public.movie"]
+            pickerController.sourceType = .photoLibrary
+            present(pickerController, animated: true, completion: nil)
+        }
     }
     
-    override var collectionViewContentSize: CGSize {
-        return contentSize
-    }
-    
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        var layoutAttributes = [UICollectionViewLayoutAttributes]()
-        
-        for attributes in self.attributed {
-            if (attributes.value as! UICollectionViewLayoutAttributes).frame.intersects(rect ) {
-                layoutAttributes.append((attributes.value as! UICollectionViewLayoutAttributes))
+    @available(iOS 14, *)
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        if let assetId = results.first?.assetIdentifier,
+           let asset = PHAsset.fetchAssets(withLocalIdentifiers: [assetId], options: nil).firstObject
+        {
+            print("asset is \(asset)")
+            PHImageManager.default().requestImageDataAndOrientation(for: asset, options: nil) { data, s, orientation, info in
+                print("\(data?.count)")
             }
         }
-        return layoutAttributes
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let originalImage = info[.originalImage] as? UIImage
+        let originalJPEGCount = originalImage?.jpegData(compressionQuality: 1.0)?.count
+        let originalPNGCount = originalImage?.pngData()?.count
+        let imageURL = info[.imageURL] as? URL
+        if let asset = info[.phAsset] as? PHAsset {
+            // Bị deprecated trên iOS 13
+            PHImageManager.default().requestImageData(for: asset, options: nil) { data, name, orientation, info in
+                print(data?.count)
+            }
+        }
+        let imageSizeFromURL = try? imageURL?.resourceValues(forKeys: [.fileSizeKey]).fileSize
+        print(info)
     }
 }
+
